424fd7c68d263fd5e67124c539a2651f0bbb06b6
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-09-30 01:36:32 +0200

    add profileDto validation + validationPipe options

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,7 +1,36 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsUUID,
+  Length,
+} from 'class-validator';
+
 export class CreateProfileDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
   userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
   firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
   lastname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 30)
   username: string;
-  avatarPath: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  avatarPath?: string;
 }
